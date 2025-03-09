a3a5e0a7d5edccfe5d16a099716d1a244e04c246
dida zidan <didazidane123@gmail.com>
2025-03-09 02:57:26 +0200

    changinf the logo to badge

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ function Navbar(){
     <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light bg-dark" >
   <div className="container-fluid">
-    <a className="text-light navbar-brand " href="#">Navbar</a>
+    <a className=" navbar-brand " href="#"><h1><span className="badge bg-light text-dark">Newsmag</span></h1></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
--- src/components/newBoard.jsx
+++ src/components/newBoard.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react"
+
 function Newboard(){
+    
     return(
     <>
-    
+    <div>
+        <h2 className="text-center">Latest <span className="badge bg-danger">news</span></h2>
+    </div>
     
     </>
     )

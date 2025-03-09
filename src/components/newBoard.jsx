c6dde3bff22a4c921715226d3b2694348bcfd25e
dida zidan <didazidane123@gmail.com>
2025-03-09 03:46:15 +0200

    fetching the api and displaying into the screen

--- src/components/newBoard.jsx
+++ src/components/newBoard.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react"
-
+import { useEffect, useState } from "react"
+import Newsitem from "./newsitem"
 function Newboard(){
-    
+    let[articles,setarticles]=useState([])
+    useEffect(()=>{
+        const apikey=import.meta.env.VITE_API_KEY
+        const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apikey}`;
+
+        fetch(url).then(response=> response.json()).then(data=>setarticles(data.articles));
+
+    },[])
     return(
     <>
-    <div>
         <h2 className="text-center">Latest <span className="badge bg-danger">news</span></h2>
-    </div>
+        <div className="container">
+            {articles.map((news,index)=>{
+                return <Newsitem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+            })}
+        </div>
     
     </>
     )
--- src/components/newsitem.jsx
+++ src/components/newsitem.jsx
@@ -1,8 +1,14 @@
-function Newsitem(){
+function Newsitem({title,description,src,url}){
     return(
     <>
-    
-    
+    <div className="bg-dark text-light card d-inline-block my-3 mx-3 py-3 px-3 mb-3 p-3 styles" style={{maxWidth:'345px'}}>
+        <img  src={src} className="card-img-top " alt="..."/>
+        <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href={url} target="_blank" className="btn btn-light targetbtn">Read more</a>
+        </div>
+    </div>
     </>
     )
 }

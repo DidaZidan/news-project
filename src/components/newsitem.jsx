2042bc9e73cf5488e2efa8f498c87042f4a49aa4
DidaZidan <didazidane123@gmail.com>
2025-03-16 23:45:29 +0200

    not displayed

--- src/components/newsitem.jsx
+++ src/components/newsitem.jsx
@@ -1,9 +1,10 @@
+import img from'../assets/download.png'
 
 function Newsitem({title,description,src,url}){
     return(
     <>
     <div className="bg-dark text-light card d-inline-block my-3 mx-3 py-3 px-3 mb-3 p-3 styles" style={{maxWidth:'345px'}}>
-        <img  src={src} className="card-img-top " alt="..."/>
+        <img  src={src?src:img} className="card-img-top " alt="..."/>
         <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
